f5744a8e7f655730487c4cd2a1151658f431b8ba
jago425 <rmjagodowski@gmail.com>
2017-11-03 11:26:17 -0400

    Adds Upload.create to promise chain
    Adds the creation of an Upload to the promise chain that exists at the
    bottom of the bin/script. Uses the mongoose model and adds a new
    `Upload` to the DB

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -1,6 +1,9 @@
 'use strict'
 
+const mongoose = require('../app/middleware/mongoose')
+
 const s3Upload = require('../lib/aws-s3-upload')
+const Upload = require('../app/models/upload')
 
 const file = {
   path: process.argv[2],
@@ -8,5 +11,14 @@ const file = {
 }
 
 s3Upload(file)
-  .then((s3Response) => console.log('s3 response is ?', s3Response))
+  .then((s3Response) => {
+    console.log('location is ?', s3Response)
+    return Upload.create({
+      description: 'does this work?',
+      url: 'what is the url?'
+    })
+  })
+  // .then(console.log) thise is the same as the line below
+  .then((whateverisbeingreturned) => console.log('return val is', whateverisbeingreturned))
   .catch(console.error)
+  .then(() => mongoose.connection.close())
